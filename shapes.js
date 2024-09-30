f85cbb131458159e91bb6d61ac0256fffc61aef7
Chivito1028 <Martin1028v@gmail.com>
2024-09-29 17:18:15 -0700

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -4,9 +4,26 @@ let canvas = document.getElementById("webgl-canvas");
 gl = canvas.getContext("webgl2");
 if (!gl) { alert("Your Web browser doesn't support WebGL 2\nPlease contact
 Dave"); }
-// Add initialization code here
+gl.enable(gl.enable(gl.DEPTH_TEST);
+let axes = new Axes(gl);
+let ms = new MatrixStack();
+let angle = 0.0;
+        let render = () => {
+          gl.clear(gl.COLOR_BUFFER_BIT);
+          angle += 3.0;
+          angle %= 360.0;
+
+          ms.push();
+          ms.rotate(angle [1,1,0]);
+          axes.draw();
+          ms.pop();
+
+          requestAnimationFrame(render);
+        };
+  //add initialization code here
 }
 function render() {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 // Add rendering code here
 }
 window.onload = init;
