2f0f42c78422fc5b1ab60a6675b00c31d75773d2
Chivito1028 <Martin1028v@gmail.com>
2024-09-29 17:48:07 -0700

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -5,9 +5,10 @@ gl = canvas.getContext("webgl2");
 if (!gl) { alert("Your Web browser doesn't support WebGL 2\nPlease contact
 Dave"); }
 gl.enable(gl.enable(gl.DEPTH_TEST);
-let axes = new Axes(gl);
+let axes = new Axes(gl;
 let ms = new MatrixStack();
 let angle = 0.0;
+          //axel
         let render = () => {
           gl.clear(gl.COLOR_BUFFER_BIT);
           angle += 3.0;
@@ -20,6 +21,22 @@ let angle = 0.0;
 
           requestAnimationFrame(render);
         };
+let sphere = new Sphere(gl,36);
+let mss = new MatrixStack();
+let angle = 25;
+          let render = () {
+                  gl.clear(gl.COLOR_BUFFER_BIT);
+                  angle += 5;
+                  angle %= 180;
+
+                  ms.push();
+                  ms.rotate(angle,[0,1,1]);
+                  ms.scale(1);
+                  cone.mv = ms.current();
+                  cone.draw();
+                  ms.pop();
+
+                  requestAnimationFrame(render);
   //add initialization code here
 }
 function render() {
