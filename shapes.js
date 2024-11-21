abec08b8d6f046f108ce9b11e732385b40a2afb5
Chivito1028 <Martin1028v@gmail.com>
2024-11-20 20:30:06 -0800

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -1,14 +1,17 @@
+"use strict;"
 let gl = undefined;
 function init() {
 let canvas = document.getElementById("webgl-canvas");
 gl = canvas.getContext("webgl2");
-if (!gl) { alert("Your Web browser doesn't support WebGL 2\nPlease contact
-Dave"); }
-gl.enable(gl.enable(gl.DEPTH_TEST));
-let axes = new Axes(gl;
+gl.clearColor(1,0,0,1);
+
+//if (!gl) { alert("Your Web browser doesn't support WebGL 2\nPlease contact
+//Dave"); }
+gl.enable(gl.DEPTH_TEST);
+let axes = new Axes(gl);
 let ms = new MatrixStack();
 let angle = 0.0;
-let sphere = new Sphere(gl,36);
+let sphere = new Sphere(gl,36,18);
 let cone = new Cone(gl,36);
 let sangle = 25;
 let cangle = 25;
@@ -21,32 +24,41 @@ let cangle = 25;
 
           ms.push();
           ms.rotate(angle, [1,1,0]);
+	  axes.MV = ms.current();
           axes.draw();
           ms.pop();
                   
-          gl.clear(gl.COLOR_BUFFER_BIT);
+         
           sangle += 5;
-          sangle %= 180;
-
+          sangle %= 360;
+			
           ms.push();
-          ms.rotate(sangle,[0,1,1]);
-          ms.scale(1);
-          cone.mv = ms.current();
-          cone.draw();
+
+         // ms.rotate(sangle,[0,1,1]);
+	  ms.translate(.5,.7,0);
+          ms.scale(0.2,0.2,0.2);
+	  	
+          sphere.MV = ms.current();
+	  	
+
+          sphere.draw();
           ms.pop();
 
                 
   //add initialization code here
 
-          gl.clear(gl.COLOR_BUFFER_BIT);
-          cangle += 5;
-          cangle %= 180;
+        
+          cangle += 1;
+          cangle %= 360;
 
           ms.push();
-          ms.rotate(cangle,[0,1,1]);
-          ms.scale(1);
-          cone.mv = ms.current();
-          cone.draw();
+          //ms.rotate(cangle,[0,0,0.1]);
+	  ms.translate(.5,-.7,0);
+
+          ms.scale(0.1);
+          cone.MV = ms.current();
+
+         cone.draw();
           ms.pop();
 
           requestAnimationFrame(render);
